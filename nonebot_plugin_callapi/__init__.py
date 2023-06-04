9aadbaffc47af1dde2a6dc1e0c3cde73efe2d175
student_2333 <lgc2333@126.com>
2023-06-05 01:05:34 +0800

    🎉 NoneBot 2.0 🚀

--- nonebot_plugin_callapi/__init__.py
+++ nonebot_plugin_callapi/__init__.py
@@ -3,11 +3,14 @@ from nonebot.plugin import PluginMetadata
 from .__main__ import HELP_TEXT
 from .config import ConfigModel
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __plugin_meta__ = PluginMetadata(
-    "CallAPI",
-    "使用指令来调用 Bot 的 API",
-    HELP_TEXT,
-    ConfigModel,
-    {"License": "MIT", "Author": "student_2333"},
+    name="CallAPI",
+    description="使用指令来调用 Bot 的 API",
+    usage=HELP_TEXT,
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-callapi",
+    type="application",
+    config=ConfigModel,
+    supported_adapters=None,
+    extra={"License": "MIT", "Author": "student_2333"},
 )
