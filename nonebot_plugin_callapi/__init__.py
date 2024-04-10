efbc582257914e0a37f1880f38abf75a7aa59de6
student_2333 <lgc2333@126.com>
2024-04-10 22:14:22 +0800

    up

--- nonebot_plugin_callapi/__init__.py
+++ nonebot_plugin_callapi/__init__.py
@@ -1,11 +1,11 @@
-from nonebot.plugin import PluginMetadata, require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters, require
 
-require("nonebot_plugin_saa")
+require("nonebot_plugin_alconna")
 
 from .__main__ import HELP_TEXT  # noqa: E402
 from .config import ConfigModel  # noqa: E402
 
-__version__ = "0.2.0"
+__version__ = "0.2.0.post1"
 __plugin_meta__ = PluginMetadata(
     name="CallAPI",
     description="使用指令来调用 Bot 的 API",
@@ -13,6 +13,6 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-callapi",
     type="application",
     config=ConfigModel,
-    supported_adapters=None,
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
     extra={"License": "MIT", "Author": "student_2333"},
 )
